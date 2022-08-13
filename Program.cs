153c64b7a9e61147305cc5f5b9b2b857fe1cc680
Milan Biranwar <milanbiranwar06@gmail.com>
2022-08-13 22:27:34 +0530

    Greeting Message

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+namespace HashTableandBinaryTree
+{
+    public class Program
+    {
+        public static void Main(string [] args)
+        {
+            Console.WriteLine("Wellcome to Hash Table and Binary Search Tree");
+        }
+    }
+}
